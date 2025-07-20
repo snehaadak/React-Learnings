9efaa305a81a6b9b1ab3437d81e006238a15ac11
Sneha <snehaadak321@gmail.com>
2025-07-20 23:26:46 +0530

    useEffect Hook

---
+++ Episode 6/UseEffect.md
@@ -0,0 +1,7 @@
+
+
+- What is a `monolith` architecture?
+- What is `microservices` architecture?
+- What is `useEffect`? 
+- What is `Optional Chaining`?
+- What is `Shimmer UI`?
---
+++ Episode 6/src/components/BodyComp.js
@@ -0,0 +1,43 @@
+import RestroCard from "./RestroCard.JS";
+import CuisineChoices from "./CuisineChoices";
+import { useState} from "react";
+import { useEffect } from "react";
+import resList from "../../Utilities/mockData";
+
+
+
+
+const BodyComp = () => {
+
+    const [varResList, setresList] = useState(resList);
+
+    useEffect(()=>{console.log("Use Effect called")},[]);
+    return (
+        <div className="body">
+            <div className="basicThings">
+                <div className="search">Search</div>
+                <div className="filter">
+                <button className="filter-btn" onClick={()=>{
+                    const filteredList = varResList.filter(
+                      (res)=>res.info.avgRating > 4.3);
+                     setresList(filteredList);}}> Top Rated Restuarant</button>
+                </div>
+            </div>                
+                
+            <h3>What's On Your Mind?</h3>
+            <div className="cusine-choices">
+                <CuisineChoices/>
+            </div>
+            <h3>Restuarants at your Door-steps?</h3>
+            <div className="restro-Container">
+
+                {
+                    varResList.map(restuarant => <RestroCard key={restuarant.info.id} resData = {restuarant}/>)
+                }
+            </div>
+            
+        </div>
+    );
+};
+
+export default BodyComp;
